f4467b34657121a7081e104cd6b991ec8b2d7735
myfisrt_gitdemo <bijaykumar2020in@gmail.com>
2026-04-07 13:15:30 +0530

    for loops in python

--- .vscode/Loops.py
+++ .vscode/Loops.py
@@ -12,23 +12,23 @@ for i in range(1,num,1):
     print(i);
 
 #[Q3] reverse of a number
-
+ 
 num1 = int(input("Enter a number : "));
 for i in range(num1,0,-1):
     print(i); 
-
+ 
 #[Q4] Table of a number
 num2 = int(input("Enter a number : "));
 for i in range(1,11,1):
     print(num2*i); 
-
+ 
 #[Q5] sum of n natural numbers
 num3 = int(input("Enter a number : "));
 sum = 0
 for i in range(1,num3+1,1):
     sum += i;
 print("Sum of n natural numbers is : ",sum); 
-
+ 
 #[Q6] factorial of a number
 num4 = int(input("Enter a number : "));
 fact = 1
@@ -36,10 +36,10 @@ for i in range(1,num4+1,1):
     fact *= i;
 print("Factorial of the number is : ",fact);
  
-
+ 
 #[Q7] sum of odd and even of a number
 num5 = int(input("Enter a number : "));
-
+ 
 even = 0
 odd = 0
 for i in range(1,num5+1,1):
@@ -49,3 +49,35 @@ for i in range(1,num5+1,1):
         odd += i;
 print("Sum of even numbers is : ",even);
 print("Sum of odd numbers is : ",odd);
+
+# [Q8] factors of a number
+user1 = int(input("Enter a number : "));
+print(f"Factors of {user1} are :",end = " ");
+ 
+for i in range(1,user1+1):
+    if user1 % i == 0:
+        print(i, end=" ");
+# [Q9] perfect number
+user2 = int(input("Enter a number : "));
+
+sum = 0 
+for i in range(1,user2,1):
+    if user2 % i == 0 :
+        sum = sum + i;
+
+if sum == user2:
+    print("perfect number");
+else : 
+    print("not a perfect number");
+ 
+#[Q10] prime number
+user3 = int(input("Enter a number : "));
+count = 0
+for i in range(1,user3+1,):
+    if user3 % i == 0 :
+        count = count + 1;
+if count == 2 :
+    print ("prime number");
+
+else : 
+    print("not a prime number");
